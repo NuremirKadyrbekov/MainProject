13f0af9acf70a4ee74ec777c338faf0c445773b5
Nuremir <erbolbekishovic@gmail.com>
2023-12-27 21:08:00 +0600

    add com

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
+import { useState } from "react";
+import Munara from "./components/Munara/Munara";
+import './components/Munara/Munara.css';
+import IMG from './components/Munara/141.jpg'
 
 function App() {
-  return (
-    <div className="App">
+  const [Modal, setModal] = useState(false);
+
+  const openModal = () => {
+    setModal(true);
+  };
+
+  const closeModal = () => {
+    setModal(false);
+  };
      
-    </div>
+    
+ 
+  return (
+    <div>
+      
+    <button className="btn" onClick={openModal}>Открыть модальное окно</button>
+    <Munara isOpen={Modal} onClose={closeModal}>
+    <img className='img' src={IMG} alt="" />
+    </Munara> 
+
+  </div>
   );
-}
+};
+     
+
 
 export default App;
