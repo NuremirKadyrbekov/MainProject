2dd3217d367d44c1041d949b896a9fd9715ba9a4
Nuremir <erbolbekishovic@gmail.com>
2023-12-27 20:44:33 +0600

    change title

--- src/components/Elnura/Elnura.jsx
+++ src/components/Elnura/Elnura.jsx
@@ -6,7 +6,7 @@ const Elnura = () => {
     return (
         <div className="shops">
             <div className="shops-left">
-                <h1>Exclusive</h1>
+                <h1></h1>
                 <h1>Offers For You</h1>
                 <p>ONLY ON BEST MEDICINE PRODUCTS</p>
                 <button>Check Now</button>
