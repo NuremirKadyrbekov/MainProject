7661ff7c8b7f284fe2f6a4ecb93f80b261349979
Alina Turdakunova <turdakunova@Alinas-MacBook-Pro.local>
2023-12-30 20:40:36 +0600

    Alina

--- src/components/Alina/Alina.jsx
+++ src/components/Alina/Alina.jsx
@@ -1,9 +1,42 @@
 import React from 'react'
+import { Carousel } from 'antd';
+import pic1 from './aaa.avif'
+import pic2 from './luna.avif'
+import pic3 from './oaoaoa.avif'
+import pic4 from './uuuuu.avif'
+import './index.css'
+ 
 
-function Alina() {
+const contentStyle = {
+    width: '500px',
+    height: '560px',
+    color: '#fff',
+    lineHeight: '160px',
+    textAlign: 'center',
+    background: '#364d79',
+  };
+  
+ export const Alina = () => {
   return (
-    <div>Alina</div>
+    <div className='main'>
+         <h1 className='text'>Do you love landscape?</h1>
+    <Carousel autoplay>
+    <div>
+      <h3 style={contentStyle}><img src={pic1} alt="" /></h3>
+      
+    </div>
+    <div>
+      <h3 style={contentStyle}><img src={pic2} alt="" /></h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}><img src={pic4} alt="" /></h3>
+    </div>
+    <div>
+      <h3 style={contentStyle}><img src={pic3} alt="" /></h3>
+    </div>
+  </Carousel>
+    </div>
   )
 }
 
-export default Alina
+
